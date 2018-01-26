0521b7d765e2281e93285d95953aeddb269c2535
sagar <sagar@sagars-MacBook-Pro.local>
2018-01-26 21:16:24 +0530

    initial commit

---
+++ first app xcode 9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  first app xcode 9
+//
+//  Created by SAGAR BHUT on 24/01/18.
+//  Copyright © 2018 sagar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
